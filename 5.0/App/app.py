4da65fa296bf306c66c02a9d746e701a4fae6b1f
Saumya Bhatt <saumya.bhatt106@gmail.com>
2020-11-10 22:50:07 +0530

    Added session manager to 5.0

--- 5.0/App/app.py
+++ 5.0/App/app.py
@@ -1,14 +1,18 @@
+from os import stat
 import streamlit as st
 import pandas as pd
-
-import sqlite3
 #st.set_page_config(layout="wide")
 
-from frame import newline, sqlQueries
+from frame import newline, sqlQueries, SessionManager
+from streamlit_metrics import metric_row
+
 
 st.title('CSIR - National Institute of Oceanography')
 st.subheader('Marine AUV Dashboard')
 
+
+status = st.sidebar.empty()
+session = SessionManager('sessionManager.db')
 AUV_BOT_NAME = ''
 AUV_BOT_ID = ''
 
@@ -16,7 +20,7 @@ def loginInstance():
 
     global AUV_BOT_NAME, AUV_BOT_ID
 
-    st.write("Generate, set or delete any AUV instances from this panel. Dashboard's functionalities cannot be used if an instance is not set.")
+    st.write("Generate, set or delete any AUV instances from this panel. Dashboard's functionalities cannot be accessed if an instance is not set.")
 
     col1, col2= st.beta_columns(2)
     temp_name = col1.text_input('Enter AUV bot name :',key='auvBotName')
@@ -25,18 +29,15 @@ def loginInstance():
 
     if submit:
         if temp_name == '':
-            st.error('Please give a name to the AUV bot instance!')
+            status.error('Please give a name to the AUV bot instance!')
         elif temp_id == '':
-            st.error('Please give a reference ID to the AUV bot instance!')
+            status.error('Please give a reference ID to the AUV bot instance!')
         else:
             query = "INSERT INTO instance_log VALUES (?,?,?)"
-            arg = [None,temp_name,temp_id]
-            sqlQueries(query=query, POST=True, arg=arg)
+            sqlQueries(database='instanceManager.db', query=query, POST=True, arg=[None,temp_name,temp_id])
             AUV_BOT_NAME = temp_name
             AUV_BOT_ID = temp_id
-            temp_name = ''
-            temp_id = ''
-            st.success('AUV bot instance created!')
+            status.success('AUV bot instance created!')
 
 
     delcol, instanceTable = st.beta_columns(2)
@@ -45,23 +46,22 @@ def loginInstance():
     submitDel = delcol.button('Delete AUV Instance')
     if submitDel:
         if delAUVInst == '':
-            st.error('Please enter an AUV reference ID to delete!')
+            status.error('Please enter an AUV reference ID to delete!')
         else:
             try:
                 query = "DELETE FROM instance_log WHERE auv_id=?"
-                arg = (delAUVInst,)
-                sqlQueries(query=query, POST=True, arg=arg)
-                st.success('The given AUV instance was deleted!')
+                sqlQueries(database='instanceManager.db', query=query, POST=True, arg=(delAUVInst,))
+                status.success('The given AUV instance was deleted!')
                 if delAUVInst == AUV_BOT_ID:
                     AUV_BOT_ID = ''
                     AUV_BOT_NAME = ''
                     delAUVInst = ''
             except:
-                st.error('No instance of the input reference ID is running!') 
+                status.error('No instance of the input reference ID is running!') 
 
 
     query = "SELECT * from instance_log"
-    records = sqlQueries(query=query)
+    records = sqlQueries(database='instanceManager.db', query=query)
 
 
     df = pd.DataFrame(records, columns=['ID','AUV Name','AUV Ref. ID'])
@@ -70,16 +70,64 @@ def loginInstance():
 
     newline(1)
     st.markdown('Set an AUV instance for this dashboard by specifying its reference ID :')
+    if st.button('Set session'):
+        session.createSession()
+        status.success('Session instance running!')
 
 
    
 
-instance_expander = st.beta_expander("AUV Instance generation and monitoring panel")
+instance_expander = st.beta_expander("⚙️ AUV Instance generation and monitoring panel")
 with instance_expander:
     clicked = loginInstance()
 
-
-st.sidebar.markdown('__Currently running instance : __')
+st.sidebar.header('Currently running instance :')
+sessionStatus = st.sidebar.empty()
+if session.sessionStatus():
+    sessionStatus.markdown('Session Token status : ✅ RUNNING')
+else:
+    sessionStatus.markdown('Session Token status : 🟠 NOT SET')
 st.sidebar.markdown('Name : &nbsp&nbsp&nbsp'+AUV_BOT_NAME)
-st.sidebar.markdown('Reference ID : &nbsp&nbsp'+AUV_BOT_ID) 
-st.sidebar.button('Remove Instance Pointer')
+st.sidebar.markdown('Reference ID : &nbsp&nbsp'+AUV_BOT_ID)
+if st.sidebar.button('Remove current session'):
+    if session.sessionStatus():
+        session.deleteSession()
+        sessionStatus.markdown('Session Token status : 🟠 NOT SET')
+        status.warning('Current session token was destroyed')
+    else:
+        status.info('There is no session currently in use')
+
+
+if session.sessionStatus():
+    st.sidebar.markdown('\n')
+    battery = 72
+    quote = "Battery Status : "+str(battery)+" %"
+    st.sidebar.markdown(quote)
+    st.sidebar.progress(battery)
+    st.sidebar.markdown('\n')
+    metric_row(
+        {
+            "Roll": 100,
+            "Pitch": 200,
+            "Yaw": 300,
+            "Depth": '400 m',
+        }
+    )
+
+
+
+st.sidebar.header('Navigation')
+functionality = st.sidebar.selectbox('Select functionality to open console',('None','Real-time Mapping','Mission File Upload','Onboard Camera Feed'))
+if functionality == 'Real-time Mapping':
+    st.header('Real-time Mapping')
+elif functionality == 'Mission File Upload':
+    st.header('Mission File Upload')
+elif functionality == 'Onboard Camera Feed':
+    st.header('Onboard Camera Feed')
+else:
+    pass
+
+
+st.sidebar.markdown('\n')
+st.sidebar.markdown('_Press if dashboard overloads_')
+st.sidebar.button('Kill all processes')
--- 5.0/App/frame.py
+++ 5.0/App/frame.py
@@ -1,14 +1,52 @@
 import streamlit as st
-
 import sqlite3
+
+
+class SessionManager():
+    live = False
+
+    def __init__(self, database):
+        self.database = database
+    
+    def getLastRow(self):
+        query = 'SELECT * FROM sessions ORDER BY id DESC LIMIT 1'
+        return sqlQueries(database=self.database, query=query)
+
+    def sessionStatus(self):
+        self.live = False
+        row = self.getLastRow()
+        try:
+            if row[0][1] == 1 : self.live = True
+            return self.live
+        except:
+            return self.live
+
+    def createSession(self):
+        query = 'INSERT INTO sessions VALUES (?,?)'
+        sqlQueries(database=self.database, query=query, POST=True, arg=(None,1))
+
+    def deleteSession(self):
+        try:
+            row = self.getLastRow()
+            query_update = 'UPDATE sessions SET session_status = 0 WHERE id = '+str(row[0][0])
+            sqlQueries(database=self.database, query=query_update, override=True)
+        except:
+            pass
+
 
 def newline(x):
     for _ in range(x):
         st.markdown('\n')
 
-def sqlQueries(query,POST=False,arg=None):
-    conn = sqlite3.connect('instanceManager.db')
+def sqlQueries(database,query,POST=False,arg=None,override=False):
+    conn = sqlite3.connect(database)
     curr = conn.cursor()
+
+    if override:
+        curr.execute(query)
+        conn.commit()
+        conn.close()
+        return None
 
     if POST == False:
         curr.execute(query)
